e9b9c13e2f157a9377c08f29d4791ce035740992
Narcis <narcis.hulband@gmail.com>
2023-02-16 15:49:28 +0200

    add placeholder-page for news-page

--- src/app/core/main/home/home-page/news-page/news-page-placeholder/news-page-placeholder.component.ts
+++ src/app/core/main/home/home-page/news-page/news-page-placeholder/news-page-placeholder.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-news-page-placeholder',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./news-page-placeholder.component.scss']
 })
 export class NewsPagePlaceholderComponent {
-
+  public placeholderCards: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 }
